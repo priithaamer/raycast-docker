64df8a12949ef7496103d147d8a585e08695a907
Priit Haamer <priit@haamer.ee>
2021-10-17 12:40:59 +0300

    feat: first working version of container list

--- src/container_list.tsx
+++ src/container_list.tsx
@@ -1,23 +1,80 @@
-import { List } from '@raycast/api';
+import { ActionPanel, Color, Detail, Icon, List } from '@raycast/api';
 import { useEffect, useState } from 'react';
 import Dockerode, { ContainerInfo } from '@priithaamer/dockerode';
 
+const containerName = (container: ContainerInfo) => container.Names.map((name) => name.replace(/^\//, '')).join(', ');
+
 export default function ContainerList() {
   const [containers, setContainers] = useState<{ containers: ContainerInfo[] }>({ containers: [] });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error>();
 
   useEffect(() => {
     async function fetchContainers() {
-      const docker = new Dockerode();
-      const containers = await docker.listContainers();
-      setContainers({ containers });
+      setLoading(true);
+      try {
+        const docker = new Dockerode();
+        const containers = await docker.listContainers({ all: true });
+        setContainers({ containers });
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error);
+        }
+      } finally {
+        setLoading(false);
+      }
     }
     fetchContainers();
   }, []);
 
+  if (error) {
+    return <Detail markdown={`## Error connecting to Docker\n\n${error.message}\n`} />;
+  }
+
   return (
-    <List>
-      {containers.containers.map((container) => (
-        <List.Item key={container.Id} title={container.Names.join(', ')} />
+    <List isLoading={loading}>
+      {containers.containers.map((containerInfo) => (
+        <List.Item
+          key={containerInfo.Id}
+          title={containerName(containerInfo)}
+          subtitle={containerInfo.Image}
+          accessoryTitle={containerInfo.State}
+          icon={{ source: Icon.Terminal, tintColor: containerInfo.State === 'running' ? Color.Green : Color.Red }}
+          actions={
+            <ActionPanel>
+              {containerInfo.State === 'running' && (
+                <ActionPanel.Item
+                  title="Stop Container"
+                  onAction={async () => {
+                    const docker = new Dockerode();
+                    setLoading(true);
+                    const container = docker.getContainer(containerInfo.Id);
+                    await container.stop();
+
+                    const containers = await docker.listContainers({ all: true });
+                    setContainers({ containers });
+                    setLoading(false);
+                  }}
+                />
+              )}
+              {containerInfo.State !== 'running' && (
+                <ActionPanel.Item
+                  title="Start Container"
+                  onAction={async () => {
+                    const docker = new Dockerode();
+                    setLoading(true);
+                    const container = docker.getContainer(containerInfo.Id);
+                    await container.start();
+
+                    const containers = await docker.listContainers({ all: true });
+                    setContainers({ containers });
+                    setLoading(false);
+                  }}
+                />
+              )}
+            </ActionPanel>
+          }
+        />
       ))}
     </List>
   );
